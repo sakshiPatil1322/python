99160c5f09be451a047ba09ed12c72b9e1240803
sakshiPatil1322 <141934937+sakshiPatil1322@users.noreply.github.com>
2023-08-15 19:47:28 +0530

    Create report card.py
    Report card using python program

---
+++ report card.py
@@ -0,0 +1,19 @@
+print("  SKN SINHGAD COLLEGE OF ENGINEERING")
+print("\n\n")
+print("                       Annual Report Card \n\n")
+name=(input("      Name Of The Student:"))
+RN=(  input("      Roll no            :"))
+div=(input( "      DIV                :"))
+print("\n\n")
+print("    Enter Your Received Marks Of subjercts out of 100\n")
+m=float(input(  "    Maths              :"))
+bce=float(input("    BCE                :"))
+bme=float(input("    BME                :"))
+UHV=float(input("    UHV                :"))
+e=float(input(  "    english            :"))
+print("\n\n")
+a=500
+average=m+bce+bme+UHV+e
+print("      average            :",average)
+persentage=average/5
+print("      persentage         :",persentage)
