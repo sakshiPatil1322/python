39d9ba0fad69af826cf9b8f6ff413154df22a344
sakshiPatil1322 <141934937+sakshiPatil1322@users.noreply.github.com>
2023-09-09 22:48:25 +0530

    Create greater.py
    Greater number between 3 number.

---
+++ greater.py
@@ -0,0 +1,9 @@
+a=int(input("enter any integer:"))
+b=int(input("enter any integer:"))
+c=int(input("enter any integer:"))
+if(a>b)and(a>c):
+  print("greater number is",a)
+elif(b>a)and(b>c):
+  print("greater number is",b)
+else:
+  print("greater number is",c)
